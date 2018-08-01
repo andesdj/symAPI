c587289c0407d452f55a61b1b62ee2a07993848b
Andres <andresmontealegregiraldo@mail.com>
2018-07-31 21:20:03 -0500

    Angular 2 OK installed

--- angular/app/app.component.ts
+++ angular/app/app.component.ts
@@ -4,7 +4,7 @@ import {Component} from '@angular/core';
 // Decorador component, indicamos en que etiqueta se va a cargar la plantilla
 @Component({
     selector: 'my-app',
-    template: '<h1>Hola mundo con Angular 2 . DJ ANDES</h1>'
+    template: '<h1>Hola mundo con Angular 2 !! DJANDES</h1>'
 })
  
 // Clase del componente donde irán los datos y funcionalidades
